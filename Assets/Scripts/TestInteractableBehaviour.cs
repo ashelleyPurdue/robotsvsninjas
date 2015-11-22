176a3796240e8ea06d4b9fa6a4a3d7f3a858f3cd
Alex Shelley <ashelley@purdue.edu>
2015-11-21 19:59:59 -0500

    Added a test interactable object.

---
+++ Assets/Scripts/TestInteractableBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestInteractableBehaviour : MonoBehaviour, IInteractable
+{
+    public string GetHighlightText()
+    {
+        return "Interact with test object.";
+    }
+
+    public void Interact()
+    {
+        //Debug log something
+        Debug.Log("Interacted.");
+    }
+}
