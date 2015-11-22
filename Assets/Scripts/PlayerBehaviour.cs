26bddee8a98f0a125868ab77d3605e873543f3fc
Alex Shelley <ashelley@purdue.edu>
2015-11-21 20:22:10 -0500

    The player can now select interactables.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    public Transform myCamera;
 
-    private InteractableBehaviour highlightedObject;
+    public float interactReach = 1f;
+
+    [SerializeField]private InteractableBehaviour selectedObject;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,26 @@ public class PlayerBehaviour : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	
+        SelectObject();
 	}
+
+
+    //Misc methods
+
+    private void SelectObject()
+    {
+        //Selects an object the player is pointing at.
+
+        selectedObject = null;
+
+        RaycastHit hit;
+        if (Physics.Raycast(myCamera.position, myCamera.forward, out hit, interactReach))
+        {
+            InteractableBehaviour obj = hit.transform.GetComponent<InteractableBehaviour>();
+            if (obj != null)
+            {
+                selectedObject = obj;
+            }
+        }
+    }
 }
