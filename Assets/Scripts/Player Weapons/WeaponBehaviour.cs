de3bf65c8bb20068c6cb1a6dfbb2d5423336c233
Alex Shelley <ashelley@purdue.edu>
2015-11-23 19:40:32 -0500

    Created the abstact Weapon class, which the sword (and all other main and secondary weapons) will inherit from.

---
+++ Assets/Scripts/Player Weapons/WeaponBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class WeaponBehaviour : MonoBehaviour
+{
+    public bool attackButton = false;
+
+    public virtual void OnSwapIn()
+    {
+        //TODO: Stuff to do when this weapon is "swapped in"
+    }
+
+    public virtual void OnSwapOut()
+    {
+        //TODO: Stuff to do when this weapon is "swapped out" to another one.
+    }
+}
