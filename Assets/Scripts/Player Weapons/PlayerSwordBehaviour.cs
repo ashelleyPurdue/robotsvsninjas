e53cb57b245391546ae56efd4022c1dd5e12bdce
Alex Shelley <ashelley@purdue.edu>
2015-11-23 19:52:04 -0500

    Started on PlayerSwordBehaviour

---
+++ Assets/Scripts/Player Weapons/PlayerSwordBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSwordBehaviour : WeaponBehaviour
+{
+    public enum State {ready, swinging, recovering, coolingDown, swappedOut};
+    private State currentState = State.ready;
+
+    private float swingTime = 0.25f;
+    private float recoverTime = 0.5f;
+    private float cooldownTime = 0.2f;
+
+    private float timer = 0f;
+    private bool inFixedUpdate = false;
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
